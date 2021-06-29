383a59b8ba736a96ae5ddb7861f60e40ab2a404d
Costner Tahttadu <achmadi.jiran.tkom18@polban.ac.id>
2021-06-30 04:23:10 +0700

    commit pertama

---
+++ resources/views/client/wrong.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Wrong</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alatsi">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
+    <link rel="stylesheet" href="assets/css/untitled-1.css">
+    <link rel="stylesheet" href="assets/css/untitled-2.css">
+    <link rel="stylesheet" href="assets/css/untitled-3.css">
+    <link rel="stylesheet" href="assets/css/untitled-4.css">
+    <link rel="stylesheet" href="assets/css/untitled-5.css">
+    <link rel="stylesheet" href="assets/css/untitled-6.css">
+    <link rel="stylesheet" href="assets/css/untitled-7.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
+
+<body>
+    <h1 class="text-center text-white d-none d-lg-block site-heading"><span class="text-primary site-heading-upper mb-3" style="font-size: 25px;color: var(--blue);font-style: normal;">monitors your current clean water usage along with the cost</span><span id="Title" class="site-heading-lower" style="font-size: 53px;">waterflow monitoring system</span></h1>
+    <section>
+        <div class="container" id="contain">
+            <div class="jumbotron" id="jumbo" style="font-size: 23px;"><span style="font-size: 18px;font-style: normal;font-weight: normal;font-family: Raleway, sans-serif;margin-top: 0;text-align: center;">WATERFLOW MONITORING SYSTEM</span>
+                <h1 style="margin-top: 40px;margin-bottom: 30px;text-align: center;">Log in</h1>
+                <form style="text-align: center;"><label id="label" style="font-size: 21px;">Sorry, invalid input of username/password</label></form>
+                <p style="text-align: center;"><a class="btn btn-primary" role="button" id="log-reg" style="text-align: center;font-size: 21px;">Back</a></p>
+                <p style="font-size: 20px;text-align: center;">Dont have an account?&nbsp;&nbsp;<a id="click-link" class="register" href="Register.html">Click here</a>&nbsp;</p>
+                <p style="font-size: 20px;text-align: center;">&nbsp;Admin? please login&nbsp;<a id="click-link" class="admin" href="Admin_Login.html">&nbsp;here</a>&nbsp;</p>
+            </div>
+        </div>
+    </section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/current-day.js"></script>
+</body>
+
+</html>
