dacb957c642ebc8963e310567653d5a02c0d74f0
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 06:24:50 +0700

    nullable foreignId

--- database/migrations/2021_07_03_201000_create_users_table.php
+++ database/migrations/2021_07_03_201000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles');
-            $table->foreignId('profile_id')->constrained('profiles');
+            $table->foreignId('role_id')->nullable()->constrained('roles');
+            $table->foreignId('profile_id')->nullable()->constrained('profiles');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
