259ad7109487e7b89009cc332ac5ac9c62d0f72b
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 06:40:33 +0700

    asset helper

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,21 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Admin Account</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alatsi">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
-    <link rel="stylesheet" href="assets/css/untitled-1.css">
-    <link rel="stylesheet" href="assets/css/untitled-2.css">
-    <link rel="stylesheet" href="assets/css/untitled-3.css">
-    <link rel="stylesheet" href="assets/css/untitled-4.css">
-    <link rel="stylesheet" href="assets/css/untitled-5.css">
-    <link rel="stylesheet" href="assets/css/untitled-6.css">
-    <link rel="stylesheet" href="assets/css/untitled-7.css">
-    <link rel="stylesheet" href="assets/css/untitled.css">
+    <link rel="stylesheet" href="{{ asset('/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Pretty-Footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-1.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-2.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-3.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-4.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-5.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-6.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled-7.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/untitled.css') }}">
 </head>
 
 <body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url('assets/img/bg2.jpg');">
