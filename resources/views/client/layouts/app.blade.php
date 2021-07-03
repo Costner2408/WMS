ec96044c00a11f82786f44880593bb6b7ca1f1e7
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 06:58:34 +0700

    URL Helper

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -5,24 +5,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Account </title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alatsi">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
-    <link rel="stylesheet" href="assets/css/untitled-1.css">
-    <link rel="stylesheet" href="assets/css/untitled-2.css">
-    <link rel="stylesheet" href="assets/css/untitled-3.css">
-    <link rel="stylesheet" href="assets/css/untitled-4.css">
-    <link rel="stylesheet" href="assets/css/untitled-5.css">
-    <link rel="stylesheet" href="assets/css/untitled-6.css">
-    <link rel="stylesheet" href="assets/css/untitled-7.css">
-    <link rel="stylesheet" href="assets/css/untitled.css">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Pretty-Footer.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-1.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-2.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-3.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-4.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-5.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-6.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-7.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled.css') }}">
 </head>
 
-<body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url('assets/img/bg2.jpg');">
+<body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), {{ URL::asset('assets/img/bg2.jpg') }};">
 <h1 class="text-center text-white d-none d-lg-block site-heading" id="title-heading"><span class="text-primary site-heading-upper mb-3" style="font-size: 25px;color: var(--blue);font-style: normal;">monitors your current clean water usage along with the cost</span><span id="Title" class="site-heading-lower" style="font-size: 59px;">waterflow monitoring system</span></h1>
 
 @include('client.layouts.header')
@@ -35,9 +35,9 @@
 
 @include('client.layouts.footer')
 
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/js/current-day.js"></script>
+<script src="{{ URL::asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/current-day.js') }}"></script>
 </body>
 
 </html>
