8ea06c96c2dbbd5b4dc34a3ce0a4c5893f3252fb
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 06:30:29 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminTableSeeder::class,
             RoleTableSeeder::class,
+            AdminTableSeeder::class,
         ]);
     }
 }
