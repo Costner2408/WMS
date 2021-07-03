e4028f90c2ed6b81152ddc5aa63b57008f27bf59
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 06:50:45 +0700

    URL

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,21 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Admin Account</title>
-    <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alatsi">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
-    <link rel="stylesheet" href="{{ URL::asset('fonts/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/Pretty-Footer.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-1.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-2.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-3.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-4.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-5.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-6.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled-7.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/untitled.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Pretty-Footer.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-1.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-2.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-3.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-4.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-5.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-6.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled-7.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/untitled.css') }}">
 </head>
 
 <body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url('assets/img/bg2.jpg');">
@@ -35,9 +35,9 @@
 
 @include('admin.layouts.footer')
 
-<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
-<script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('js/current-day.js') }}"></script>
+<script src="{{ URL::asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/current-day.js') }}"></script>
 </body>
 
 </html>
