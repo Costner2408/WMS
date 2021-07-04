fd05e25ff3b66c043861b1c2a6cfd6e2a58ace41
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-04 07:19:16 +0700

    add logout lin

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -7,6 +7,17 @@
                 <li class="nav-item"><a class="nav-link" href="{{ route('admin.profile') }}">Profile</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('admin.complaint') }}">Complaints</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('admin.sms') }}">SMS</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
             </ul>
         </div><button data-target="#navbarResponsive" data-toggle="collapse" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-align-justify"></i></button>
     </div>
